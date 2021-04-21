348247949fa29528eb570b02645b0c8439f4842a
derekrussell <derekrussell@icloud.com>
2021-04-21 22:11:05 +0100

    Initial migration of Codepen project.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/images");
   return {
     dir: {
       input: "src",
